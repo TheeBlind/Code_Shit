2c85d9cb8796a06b8403c46e60f94830b1a1f49b
o00o <26583110+TheeBlind@users.noreply.github.com>
2018-12-06 23:38:16 +0100

    Update DomAins_Checker.sh
    -W	Specifies how long to wait for a reply (10 sec)
    -s	A SERVFAIL response should stop query

--- DomAins_Checker.sh
+++ DomAins_Checker.sh
@@ -7,7 +7,7 @@ while read -r line; do
 
 List=$(echo $line|tr -d '\r')
 
-if [[ $(host $line | grep -oP "has address") ]];
+if [[ $(host -s -W 10 $line | grep -oP "has address") ]];
 then echo $(tput setaf 4)$line$(tput sgr 0) "-----------> $(tput setaf 2)ALIVE$(tput sgr 0)" && echo $line >> Alive.txt
 else echo $(tput setaf 4)$line$(tput sgr 0) "-----------> $(tput setaf 1)DEATH$(tput sgr 0)" && echo $line >> Death.txt
 fi
