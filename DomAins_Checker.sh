103aa475d559d7814a77ebfa437b4a4b2b8ceec9
o00o <26583110+TheeBlind@users.noreply.github.com>
2018-12-13 14:58:02 +0100

    Update DomAins_Checker.sh
    added threads, now is fast ;)

--- DomAins_Checker.sh
+++ DomAins_Checker.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-# Check if domains are  alive
+#Domain Checker
 
-DOMAINS="/root/your_domain_list.txt"
+DOMAINS="your_domain_list.txt"
 
-while read -r line; do
-
-List=$(echo $line|tr -d '\r')
-
-if [[ $(host -s -W 10 $line | grep -oP "has address") ]];
-then echo $(tput setaf 4)$line$(tput sgr 0) "-----------> $(tput setaf 2)ALIVE$(tput sgr 0)" && echo $line >> Alive.txt
-else echo $(tput setaf 4)$line$(tput sgr 0) "-----------> $(tput setaf 1)DEATH$(tput sgr 0)" && echo $line >> Death.txt
+check() {
+if [[ $(host -s -W 10 $1 | grep -oP 'has address') ]]; then
+    echo $(tput setaf 4)$1$(tput sgr 0) "-----------> $(tput setaf 2)ALIVE$(tput sgr 0)" && echo $line >> Alive.txt;
+else
+    echo $(tput setaf 4)$1$(tput sgr 0) "-----------> $(tput setaf 1)DEATH$(tput sgr 0)" && echo $line >> Death.txt;
 fi
-done < $DOMAINS
+}
+export -f check
+cat $DOMAINS | parallel -j "$(nproc)" check {}
+
