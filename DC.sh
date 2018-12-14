e8ac354ab86de5da790da1a6d25b12f8bcde2a9d
o00o <26583110+TheeBlind@users.noreply.github.com>
2018-12-14 13:30:35 +0100

    Update DC.sh

--- DC.sh
+++ DC.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #Domain Checker
 
-DOMAINS="your_domain_list.txt"
+DOMAINS="/home/theblind/Desktop/miss.txt"
 
 check() {
 if [[ $(host -s -W 10 $1 | grep -oP 'has address') ]]; then
-    echo $(tput setaf 4)$1$(tput sgr 0) "-----------> $(tput setaf 2)ALIVE$(tput sgr 0)" && echo $line >> Alive.txt;
+    echo $(tput setaf 4)$1$(tput sgr 0) "-----------> $(tput setaf 2)ALIVE$(tput sgr 0)" && echo $1 >> Alive.txt;
 else
-    echo $(tput setaf 4)$1$(tput sgr 0) "-----------> $(tput setaf 1)DEATH$(tput sgr 0)" && echo $line >> Death.txt;
+    echo $(tput setaf 4)$1$(tput sgr 0) "-----------> $(tput setaf 1)DEATH$(tput sgr 0)" && echo $1 >> Death.txt;
 fi
 }
 export -f check
-cat $DOMAINS | parallel -j "$(nproc)" check {}
+cat $DOMAINS | parallel -j 1 check {}
 
